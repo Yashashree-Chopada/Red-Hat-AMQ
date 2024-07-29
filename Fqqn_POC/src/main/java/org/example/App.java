56f94b11b31c567f39c1b5ad785ab9f979845529
Yashashree-Chopada <yashashreechopada@gmail.com>
2024-07-29 19:14:11 +0530

    fqqn-poc

---
+++ Fqqn_POC/src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javax.jms.*;
+import javax.naming.*;
+
+
+public class App {
+    public static void main(String[] args) throws NamingException, JMSException {
+        InitialContext initialContext = null;
+        JMSContext jmsContext = null;
+
+        // Obtain the InitialContext with proper JNDI properties
+        initialContext = new InitialContext();
+
+        // Lookup the ConnectionFactory from the JNDI context
+        ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
+
+        // Create JMS context
+        jmsContext = connectionFactory.createContext("admin", "admin");
+
+        // Create Topic and send a message
+        Topic topic = jmsContext.createTopic("testAddress::client1.testQueue1");
+        JMSProducer producer = jmsContext.createProducer();
+        producer.send(topic, "testMsg");
+        System.out.println("Message sent to topic");
+
+    }
+}
